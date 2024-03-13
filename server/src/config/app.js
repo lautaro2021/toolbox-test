163b97e297412556910a6bcfdbd071a5df05d8ab
lautaro2021 <lautaroarnay@gmail.com>
2024-03-13 15:10:24 -0300

    add docker

--- server/src/config/app.js
+++ server/src/config/app.js
@@ -5,9 +5,9 @@ const app = express()
 
 app.use(express.json())
 app.use((_, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    next();
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  next()
 })
 app.use(route)
 
